6e3d9a93f8167467ef1d9e1dcccf84e59903edf7
anurrraggg <anuragpandey945028@gmail.com>
2025-08-12 17:47:33 +0530

    sda

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/movie/', // repo name
+  base: '/movie/', // must be your repo name
 })
+
