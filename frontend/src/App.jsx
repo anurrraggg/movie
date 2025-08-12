8ea1232b5614214bcca10d7ed608cdf9df938622
Anurag Pandey <2k23.cs.2313929@example.com>
2025-08-12 11:35:37 +0530

    added the title and also the img to thecontest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import Movie from './component/movie'
+
 function App() {
   return (
 <div>
-  <Movie movie_card="" />
+  <Movie movie={{title: "the monkey king", url: "https://www.google.com/imgres?q=img%20for%20html&imgurl=https%3A%2F%2Fwww.copahost.com%2Fblog%2Fwp-content%2Fuploads%2F2019%2F07%2Fimgsize2.png&imgrefurl=https%3A%2F%2Fwww.copahost.com%2Fblog%2Fhtml-image%2F&docid=0h4xlDSQakK8WM&tbnid=g8ntKTZImTTAnM&vet=12ahUKEwifgLmfzISPAxXJ8jgGHX-HAfwQM3oECBwQAA..i&w=937&h=568&hcb=2&ved=2ahUKEwifgLmfzISPAxXJ8jgGHX-HAfwQM3oECBwQAA",
+    release: "2014"
+  }}/>
 </div>
   )
 }
